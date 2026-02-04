0324fcac061cb84ed760258f083e706c9dc6942a
cocecs <act.undag@gmail.com>
2026-02-04 09:01:35 +0800

    pokemon api

---
+++ fox.py
@@ -0,0 +1,10 @@
+import requests
+
+response = requests.get("http://randomfox.ca/floof")
+# print(response.status_code)
+# print(response.text)
+# print(response.json())
+
+
+fox = response.json()
+print(fox['image'])
---
+++ poke-api.py
@@ -0,0 +1,27 @@
+import requests
+
+api_url = "https://pokeapi.co/api/v2/"
+
+
+def get_pokemon_info(name):
+    url = f"{api_url}/pokemon/{name}"
+    response = requests.get(url)
+    # print(response)
+
+
+    if response.status_code == 200:
+        pokemon_data = response.json()
+        return pokemon_data
+    else:
+        print(f"Failed to retrieved data. {response.status_code}")
+
+
+pokemon_name = "pikachu"
+pokemon_info = get_pokemon_info(pokemon_name)
+
+# display data
+if pokemon_info:
+    print(f"{pokemon_info["name"]}")
+    print(f"{pokemon_info["height"]}")
+    print(f"{pokemon_info["weight"]}")
+    print(f"{pokemon_info["base_experience"]}")
